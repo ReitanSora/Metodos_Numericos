04a980a0b9f946c4edfcdb529a4ecf39112d79ab
ReitanSora <stivenpilca@gmail.com>
2023-05-22 19:16:28 -0500

    Se sube archivo 1 y 2, Control de Flujo

--- UNIDAD_1/SISTEMAS_NUMERACION/ejercicio1.py
+++ UNIDAD_1/SISTEMAS_NUMERACION/ejercicio1.py
@@ -1,13 +1,13 @@
-#DE BINARIO A DECIMAL
+# DE BINARIO A DECIMAL
 
-binario=101111
-decimal=0
-i=0
+binario = 101111
+decimal = 0
+i = 0
 
-while(binario>0):
-    digito =binario%10
-    binario= int(binario/10)
-    decimal= decimal+digito*(2**i)
-    i= i+1
+while (binario > 0):
+    digito = binario % 10
+    binario = int(binario/10)
+    decimal = decimal + digito*(2**i)
+    i += 1
 
 print("El número en decimal es: "+str(decimal))
--- UNIDAD_1/SISTEMAS_NUMERACION/ejercicio2.py
+++ UNIDAD_1/SISTEMAS_NUMERACION/ejercicio2.py
@@ -1,13 +1,13 @@
-#DE DECIMAL A BINARIO
+# DE DECIMAL A BINARIO
 
-decimal= 27025
-binario=0
-i=0
+decimal = 27025
+binario = 0
+i = 0
 
-while(decimal>0):
-    residuo=decimal%2
-    binario+= residuo *(10**i)
-    decimal //=2
-    i+=1
+while (decimal > 0):
+    residuo = decimal % 2
+    decimal = int(decimal/2)
+    binario = binario + residuo * (10**i)
+    i += 1
 
 print("El valor de decimal a binario es: "+str(binario))
