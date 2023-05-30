d860b3b28ef3fb3afbd1b92375224c16854a3981
ReitanSora <stivenpilca@gmail.com>
2023-05-29 23:14:31 -0500

    Se agrega comentarios y pocas correcciones al codigo

--- UNIDAD_1/PUNTO_FLOTANTE/ejercicio12.py
+++ UNIDAD_1/PUNTO_FLOTANTE/ejercicio12.py
@@ -6,6 +6,7 @@
 # B = (float) A;
 # C = (int) A;
 
+# Metodo para transformar un numero decimal a binario
 def decimal_a_binario(decimal):
     binario = 0
     i = 0
@@ -19,6 +20,7 @@ def decimal_a_binario(decimal):
     return binario
 
 
+# Metodo para transformar un numero binario a decimal
 def binario_a_decimal(binario):
     decimal = 0
     i = 0
@@ -32,6 +34,7 @@ def binario_a_decimal(binario):
     return decimal
 
 
+# Metodo para buscar el primer 1 y colocar una coma después de ese 1
 def busqueda_puesta_punto(num_binario):
     texto_num = str(num_binario)
     posicion = int(texto_num.find("1"))
@@ -41,6 +44,7 @@ def busqueda_puesta_punto(num_binario):
     return resultado
 
 
+# Metodo para quitar la coma
 def quitar_punto(texto):
     posicion = int(texto.find(","))
     resultado = texto[:posicion]+texto[posicion+1:]
@@ -48,6 +52,7 @@ def quitar_punto(texto):
     return resultado
 
 
+# Metodo para quitar el primer 1
 def quitar_uno(numero):
     posicion = int(numero.find("1"))
     resultado = numero[:posicion]+numero[posicion+1:]
@@ -55,8 +60,8 @@ def quitar_uno(numero):
     return resultado
 
 
+# Metodo para calcular el signo desde un numero decimal
 def signo(num_decimal):
-    signo_binario = ""
 
     if num_decimal >= 0:
         signo_binario = "0"
@@ -66,11 +71,13 @@ def signo(num_decimal):
     return signo_binario
 
 
+# Metodo para calcular el exponente de los 2 tipos de precisiones
 def exponente(num_binario, tipo_pre):
     texto_proceso = busqueda_puesta_punto(num_binario)
     posicion = int(texto_proceso.find(","))
     num_exponente = int(len(texto_proceso[posicion+1:]))
 
+    # Proceso para calcular el exponente en un tipo de precision u otro
     if tipo_pre == 1:
         exp_binario = 127 + num_exponente
     else:
@@ -81,14 +88,17 @@ def exponente(num_binario, tipo_pre):
     return texto_final
 
 
+# Metodo para calcular la mantisa de los 2 tipos de precisiones
 def mantissa(num_binario, tipo_pre):
     digitos_i = int(len(str(num_binario)))
 
+    # Proceso para calcular si faltan o sobran digitos
     if tipo_pre == 1:
         num = 24-digitos_i
     else:
         num = 53-digitos_i
 
+    # Proceso para calcular la mantissa en un tipo de precision u otro
     if num >= 0:
         mant_sp = str(num_binario)+","+"0"*num
         mant_p = quitar_punto(mant_sp)
@@ -103,19 +113,19 @@ def mantissa(num_binario, tipo_pre):
     return mant_f
 
 
+# Metodo para representar un numero binario, sin normalizar, normalizado y transformado a hexadecimal
 def norma_bin_hexa(num_dec, num_bin):
     num_digitos = len(str(num_bin))
     num_resultado = ""
     contador = 0
 
+    # Mostrar numero original
     print("\nVariable int\n")
     print("     Numero en binario: "+str(num_bin))
 
-    if num_dec >= 0:
-        num_signo = "0"
-    else:
-        num_signo = "1"
+    num_signo = signo(num_dec)
 
+    # Proceso para aumentar ceros o quitar digitos de ser necesario
     if num_digitos <= 31:
         num_falta = 31-num_digitos
         num_bin_f = num_signo+"0"*num_falta+str(num_bin)
@@ -130,6 +140,7 @@ def norma_bin_hexa(num_dec, num_bin):
         bin_hexa(num_bin_f, contador, num_resultado)
 
 
+# Metodo usado solamente para transformar un numero binario normalizado a hexadecimal
 def bin_hexa(num_bin_f, contador, num_resultado):
     print("     Numero normalizado: "+num_bin_f)
     while (contador < 32):
@@ -141,11 +152,13 @@ def bin_hexa(num_bin_f, contador, num_resultado):
     print("     Numero en hexadecimal: "+num_resultado)
 
 
+# Metodo que une las cadenas de texto signo, exponente y mantisa para transformarlas a hexadecimal, sirve para los dos tipos de precisiones
 def bin_a_hex(num_s_bin, num_e_bin, num_m_bin, tipo_pre):
     cadena_binario = num_s_bin+num_e_bin+num_m_bin
     contador = 0
     resultado = ""
 
+    # Proceso para calcular el numero hexadecimal para un tipo de precision u otro
     if tipo_pre == 1:
         while (contador < 32):
             binario = cadena_binario[contador:contador+4]
@@ -164,7 +177,8 @@ def bin_a_hex(num_s_bin, num_e_bin, num_m_bin, tipo_pre):
     return resultado
 
 
-def prints(num_dec, num_bin, tipo_pre):
+# Metodo que muestra los datos al usuario y hace las llamadas a los metodos anteriores
+def principal(num_dec, num_bin, tipo_pre):
     tipo = "Doble" if tipo_pre == 2 else "Simple"
     variable = "double" if tipo_pre == 2 else "float"
 
@@ -178,15 +192,16 @@ def prints(num_dec, num_bin, tipo_pre):
           exponente(num_bin, tipo_pre), mantissa(num_bin, tipo_pre), tipo_pre))
 
 
-# VARIABLES
+# Variables
 A = pow(2, 28) + 5
 A_binario = decimal_a_binario(A)
 
-# PRINTS
+# Prints que muestran los datos iniciales
 print("\nValores iniciales\n")
 print("     Decimal: "+str(A))
 print("     Binario: "+str(A_binario))
 
-prints(A, A_binario, 2)
-prints(A, A_binario, 1)
+# Llamadas a metodo principal y metodo de variable int para empezar la ejecucion del programa
+principal(A, A_binario, 2)
+principal(A, A_binario, 1)
 norma_bin_hexa(A, A_binario)
