244484f5764ff5f36b8093380f925b1266882236
ReitanSora <stivenpilca@gmail.com>
2023-06-22 18:44:03 -0500

    Cambio de nombre a un atributo, ahora nickname

--- Proyecto Final/users.py
+++ Proyecto Final/users.py
@@ -1,18 +1,31 @@
+'''
+Tema: Métodos Numéricos
+#Grupo #
+#Integrantes:
+
+•	Kevin Josue Amaguaña Rivadeneira
+•	Priscila Veronica Chisag Pillajo
+•	Andy RIcardo Galarza Morales
+•	Stiven Anthony Pilca Sánchez
+
+#Carrera: Ingeniería en Sistemas de la información
+#Paralelo: SI4 - 002
+'''
+
 class Usuarios():
 
-    def __init__(self, nombre, apellido, nombre_usuario, correo_electronico, clave):
+    def __init__(self, nombre, apellido, nickname, correo_electronico, clave):
         self.nombre = nombre
         self.apellido = apellido
-        self.nombre_usuario = nombre_usuario
+        self.nickname = nickname
         self.correo_electronico = correo_electronico
         self.clave = clave
 
-    def crear (self, nombre: str, apellido: str, nombre_usuario: str, correo_electronico: str, clave: str):
+    def crear(self, nombre: str, apellido: str, nickname: str, correo_electronico: str, clave: str):
 
-        usuario = Usuarios(nombre, apellido, nombre_usuario, correo_electronico, clave)
+        usuario = Usuarios(nombre, apellido, nickname,
+                           correo_electronico, clave)
 
         usuarios = list()
         usuarios.append(usuario)
         print(usuarios)
-
-
