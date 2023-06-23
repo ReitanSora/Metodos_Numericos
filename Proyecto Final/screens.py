713ba2dca6b4e5bf594c37ff343bbdcd0ac8bb0b
ReitanSora <stivenpilca@gmail.com>
2023-06-23 02:41:03 -0500

    Primer diseño de la ventana propagación de errores

--- Proyecto Final/screens.py
+++ Proyecto Final/screens.py
@@ -13,6 +13,7 @@ Tema: Métodos Numéricos
 '''
 
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import random as r
 from static import style
@@ -32,10 +33,10 @@ class Home(tk.Frame):
         self.init_widgets()
 
     def move_to_errores(self):
-        self.controller.show_frame(Burbuja)
+        self.controller.show_frame(PropErrores)
 
     def move_to_sistemas(self):
-        self.controller.show_frame(Insercion)
+        self.controller.show_frame()
 
     def move_to_flotante(self):
         self.controller.show_frame(PuntoFlotante)
@@ -355,6 +356,100 @@ class Burbuja(tk.Frame):
         boton_5.bind('<Leave>', event.on_leave)
 
 
+class PropErrores(tk.Frame):
+
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.configure(background=style.BG)
+        self.controller = controller
+        self.init_widgets()
+
+    def move_to_home(self):
+        self.controller.show_frame(Home)
+
+    def init_widgets(self):
+        tk.Label(self,
+                 text="Propagación de Errores",
+                 **style.STYLE_TITTLE,
+                 ).pack(side=tk.TOP, fill=tk.BOTH, pady=30)
+
+        inputFrame = tk.Frame(self, background=style.BG)
+        inputFrame.columnconfigure(0, weight=1)
+        inputFrame.pack(side=tk.TOP, fill=tk.BOTH)
+
+        # label ecuacion
+        tk.Label(inputFrame,
+                 text="Ecuación",
+                 **style.STYLE_SUBTITTLE,
+                 ).grid(row=0, column=0)
+
+        # label exponente
+        tk.Label(inputFrame,
+                 text="Exponente",
+                 **style.STYLE_SUBTITTLE,
+                 ).grid(row=1, column=0)
+
+        # label respuesta
+        tk.Label(inputFrame,
+                 text="Respuesta",
+                 **style.STYLE_SUBTITTLE,
+                 ).grid(row=2, column=0)
+
+        # entry desactivado formula
+        self.formula = tk.StringVar()
+        tk.Entry(inputFrame,
+                 textvariable=self.formula,
+                 **style.STYLE_ENTRY_DES,
+                 ).grid(row=0, column=1, pady="10", padx="20")
+
+        # entry desactivado respuesta
+        self.respuesta = tk.StringVar()
+        tk.Entry(inputFrame,
+                 textvariable=self.respuesta,
+                 **style.STYLE_ENTRY_DES,
+                 ).grid(row=2, column=1, pady="10", padx="20")
+
+        # entry desactivado exponente
+        self.exponente = tk.StringVar()
+        tk.Entry(inputFrame,
+                 textvariable=self.exponente,
+                 **style.STYLE_ENTRY,
+                 ).grid(row=1, column=1, pady="10", padx="20")
+
+        # boton para calcular
+        borde_1 = tk.LabelFrame(inputFrame,
+                                **style.STYLE_BUTTON_BORDER)
+        borde_1.grid(row=0, rowspan=3, column=2, pady="30", padx="20")
+
+        boton_calculo = tk.Button(borde_1,
+                                  text="Calcular",
+                                  **style.STYLE_BUTTON,
+                                  )
+        boton_calculo.pack(side=tk.TOP, fill=tk.BOTH, expand=True, pady=0)
+
+        boton_calculo.bind('<Enter>', event.on_enter)
+        boton_calculo.bind('<Leave>', event.on_leave)
+
+        outputFrame = tk.Frame(self, background="#666666")
+        outputFrame.pack(side=tk.TOP, fill=tk.BOTH,
+                         expand=True, padx="10", pady="10")
+
+        # boton para regresar
+        borde_2 = tk.LabelFrame(self,
+                                **style.STYLE_BUTTON_RETURN_BORDER)
+        borde_2.pack(side=tk.TOP, padx="10", pady="10")
+
+        boton_return = tk.Button(borde_2,
+                                 text="Regresar",
+                                 **style.STYLE_BUTTON_RETURN,
+                                 command=self.move_to_home
+                                 )
+        boton_return.pack(side=tk.TOP, fill=tk.BOTH, expand=True, pady=0)
+
+        boton_return.bind('<Enter>', event.on_enter_return)
+        boton_return.bind('<Leave>', event.on_leave_return)
+
+
 class PuntoFlotante(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -402,7 +497,7 @@ class PuntoFlotante(tk.Frame):
                  **style.STYLE_ENTRY,
                  ).grid(row=1, column=1, pady="20", padx="20")
 
-        # boton para registrarse
+        # boton para calcular
         borde_1 = tk.LabelFrame(inputFrame,
                                 **style.STYLE_BUTTON_BORDER)
         borde_1.grid(row=0, rowspan=2, column=2, pady="30", padx="20")
