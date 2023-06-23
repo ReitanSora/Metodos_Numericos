d45ac417d6a24e8da63602214d4a817b4694c64b
ReitanSora <stivenpilca@gmail.com>
2023-06-23 02:58:22 -0500

    Mejoras en el ahorro y corrección de código

--- Proyecto Final/register.py
+++ Proyecto Final/register.py
@@ -13,23 +13,8 @@ Tema: Métodos Numéricos
 '''
 
 import tkinter as tk
-from tkinter import messagebox
 from static import style
-
-def on_enter(e):
-    e.widget.config(**style.STYLE_BUTTON_ENTER)
-
-
-def on_leave(e):
-    e.widget.config(**style.STYLE_BUTTON)
-
-
-def on_enter_return(e):
-    e.widget.config(**style.STYLE_BUTTON_RETURN_ENTER)
-
-
-def on_leave_return(e):
-    e.widget.config(**style.STYLE_BUTTON_RETURN)
+import functions.events as event
 
 
 class Register(tk.Tk):
@@ -56,7 +41,6 @@ class Register(tk.Tk):
                  ).pack(side=tk.TOP, fill=tk.BOTH, pady="10")
 
         infoFrame = tk.Frame(self, background=style.BG)
-
         infoFrame.columnconfigure(0, weight=1)
         infoFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=True,)
 
@@ -65,71 +49,71 @@ class Register(tk.Tk):
                  text="Nombre",
                  **style.STYLE_SUBTITTLE,
                  justify="center"
-                 ).grid(row=1, column=0)
+                 ).grid(row=0, column=0)
 
         # label apellido del usuario
         tk.Label(infoFrame,
                  text="Apellido",
                  **style.STYLE_SUBTITTLE
-                 ).grid(row=2, column=0)
+                 ).grid(row=1, column=0)
 
         # label nickname
         tk.Label(infoFrame,
                  text="Nickname",
                  **style.STYLE_SUBTITTLE
-                 ).grid(row=3, column=0)
+                 ).grid(row=2, column=0)
 
         # label correo electrocnico
         tk.Label(infoFrame,
                  text="Correo",
                  **style.STYLE_SUBTITTLE
-                 ).grid(row=4, column=0)
+                 ).grid(row=3, column=0)
 
         # label clave
         tk.Label(infoFrame,
                  text="Clave",
                  **style.STYLE_SUBTITTLE
-                 ).grid(row=5, column=0)
+                 ).grid(row=4, column=0)
 
         # entry nombre usuario
         self.texto_usuario = tk.StringVar()
         tk.Entry(infoFrame,
                  textvariable=self.texto_usuario,
                  **style.STYLE_ENTRY,
-                 ).grid(row=1, column=1, pady="20", padx="20")
+                 ).grid(row=0, column=1, pady="20", padx="20")
 
         # entry apellido
         self.texto_clave = tk.StringVar()
         tk.Entry(infoFrame,
                  textvariable=self.texto_clave,
                  **style.STYLE_ENTRY
-                 ).grid(row=2, column=1, pady="20", padx="20")
+                 ).grid(row=1, column=1, pady="20", padx="20")
 
         # entry nickname
         self.texto_clave = tk.StringVar()
         tk.Entry(infoFrame,
                  textvariable=self.texto_clave,
                  **style.STYLE_ENTRY
-                 ).grid(row=3, column=1, pady="20", padx="20")
+                 ).grid(row=2, column=1, pady="20", padx="20")
 
         # entry correo
         self.texto_clave = tk.StringVar()
         tk.Entry(infoFrame,
                  textvariable=self.texto_clave,
                  **style.STYLE_ENTRY
-                 ).grid(row=4, column=1, pady="20", padx="20")
+                 ).grid(row=3, column=1, pady="20", padx="20")
 
         # entry clave
         self.texto_clave = tk.StringVar()
         tk.Entry(infoFrame,
                  textvariable=self.texto_clave,
                  **style.STYLE_ENTRY
-                 ).grid(row=5, column=1, pady="20", padx="20")
+                 ).grid(row=4, column=1, pady="20", padx="20")
 
         # boton para registrarse
         borde_1 = tk.LabelFrame(infoFrame,
                                 **style.STYLE_BUTTON_BORDER)
-        borde_1.grid(row=6, column=0, pady="30")
+        borde_1.grid(row=5, column=0, pady="30")
 
         boton_register = tk.Button(borde_1,
                                    text="Registrar",
@@ -137,13 +121,13 @@ class Register(tk.Tk):
                                    )
         boton_register.pack(side=tk.TOP, fill=tk.BOTH, expand=True, pady=0)
 
-        boton_register.bind('<Enter>', on_enter)
-        boton_register.bind('<Leave>', on_leave)
+        boton_register.bind('<Enter>', event.on_enter)
+        boton_register.bind('<Leave>', event.on_leave)
 
         # boton para regresar
         borde_2 = tk.LabelFrame(infoFrame,
                                 **style.STYLE_BUTTON_RETURN_BORDER)
-        borde_2.grid(row=6, column=1, pady="30")
+        borde_2.grid(row=5, column=1, pady="30")
 
         boton_registro = tk.Button(borde_2,
                                    text="Regresar",
@@ -152,5 +136,5 @@ class Register(tk.Tk):
                                    )
         boton_registro.pack(side=tk.TOP, fill=tk.BOTH, expand=True, pady=0)
 
-        boton_registro.bind('<Enter>', on_enter_return)
-        boton_registro.bind('<Leave>', on_leave_return)
+        boton_registro.bind('<Enter>', event.on_enter_return)
+        boton_registro.bind('<Leave>', event.on_leave_return)
