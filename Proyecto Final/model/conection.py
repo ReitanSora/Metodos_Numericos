756a6cd856a66707be2d26f08d3e1c19c62c696d
ReitanSora <stivenpilca@gmail.com>
2023-07-04 17:01:02 -0500

    Corrección ruta para el acceso a las bases de datos

--- Proyecto Final/model/conection.py
+++ Proyecto Final/model/conection.py
@@ -1,11 +1,18 @@
 import sqlite3
-
+import os
+from pathlib import Path
 
 class ConectionDB:
 
     def __init__(self):
-        self.base_datos_usuario = "./database/usuarios.db"
-        self.base_datos_persona = "./database/personas.db"
+        ruta_1 = os.path.join(os.getcwd(), 'database/usuarios.db')
+        ruta_2 = os.path.join(os.getcwd(), 'database/personas.db')
+        #print(ruta_1)
+        #print(type(ruta_1))
+        #os.makedirs(os.path.join(ruta, 'prueba'))
+        #print(os.path.exists(ruta_1))
+        self.base_datos_usuario = ruta_1
+        self.base_datos_persona = ruta_2
         self.conexion_usuario = sqlite3.connect(self.base_datos_usuario)
         self.conexion_persona = sqlite3.connect(self.base_datos_persona)
         self.cursor_usuario = self.conexion_usuario.cursor
