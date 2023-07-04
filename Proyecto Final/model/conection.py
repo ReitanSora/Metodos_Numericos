ce70656644cac6c149b249a878012d7397bedb1a
ReitanSora <stivenpilca@gmail.com>
2023-07-04 16:29:08 -0500

    Corrección ruta de las bases de datos

--- Proyecto Final/model/conection.py
+++ Proyecto Final/model/conection.py
@@ -4,8 +4,8 @@ import sqlite3
 class ConectionDB:
 
     def __init__(self):
-        self.base_datos_usuario = "database/usuarios.db"
-        self.base_datos_persona = "database/personas.db"
+        self.base_datos_usuario = "./database/usuarios.db"
+        self.base_datos_persona = "./database/personas.db"
         self.conexion_usuario = sqlite3.connect(self.base_datos_usuario)
         self.conexion_persona = sqlite3.connect(self.base_datos_persona)
         self.cursor_usuario = self.conexion_usuario.cursor
