fa65e327ce5c79e6773c9fe1ac32c794a38c9884
ReitanSora <stivenpilca@gmail.com>
2023-07-09 02:59:31 -0500

    Funcionalidad completa con gráfica embebida en un frame

--- Proyecto Final/modules/propagacion_error.py
+++ Proyecto Final/modules/propagacion_error.py
@@ -13,7 +13,10 @@ Paralelo: SI4 - 002
 '''
 
 import tkinter as tk
+import matplotlib.figure
+import numpy as np
 import functions.events as event
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from static import style
 from functions import funcion_propagacion as propagacion
 from validation import validacion
@@ -25,14 +28,29 @@ class PropErrores(tk.Frame):
         super().__init__(parent)
         self.configure(background=style.BG)
         self.controller = controller
+        
         self.init_widgets()
 
     def validar_campo(self):
-        if validacion.validate_number_float(self.exponente.get()) is True:
+        if validacion.validate_number_float(self.exponente.get()) is True and int(self.exponente.get()) > 250:
+            self.texto_alerta_exponente.set("")
             self.calcular()
+        else:
+            self.texto_alerta_exponente.set("Ingrese un valor mayor a 250")
 
     def calcular(self):
-        self.respuesta.set(propagacion.propagacion_errores(float(self.exponente.get())))
+        self.respuesta.set(propagacion.propagacion_errores(
+            int(self.exponente.get())))
+        self.graficar()
+
+    def graficar(self):
+        self.ax.clear()
+        x = np.linspace(int(self.exponente.get())-5, int(self.exponente.get())+5)
+        self.ax.scatter(int(self.exponente.get()), propagacion.propagacion_errores(int(self.exponente.get())), c=style.COLOR_BLANCO, zorder = 10)
+        self.ax.plot(x, propagacion.e(x), c=style.COLOR_MAGENTA_CLARO, zorder= 5)
+        self.ax.grid(alpha=0.3, lw=1.75, ls="--" )
+        self.canvas_figura.draw()
+        
 
     def init_widgets(self):
         tk.Label(self,
@@ -40,52 +58,78 @@ class PropErrores(tk.Frame):
                  **style.STYLE_TITTLE,
                  ).pack(side=tk.TOP, fill=tk.BOTH, pady=30)
 
-        inputFrame = tk.Frame(self, background=style.BG)
-        inputFrame.columnconfigure(0, weight=1)
-        inputFrame.pack(side=tk.TOP, fill=tk.BOTH)
+        input_frame = tk.Frame(self, background=style.BG)
+        input_frame.columnconfigure(0, weight=1)
+        input_frame.columnconfigure(1, weight=2)
+        input_frame.columnconfigure(2, weight=1)
+        input_frame.pack(side=tk.TOP, fill=tk.BOTH)
 
         # label ecuacion
-        tk.Label(inputFrame,
+        tk.Label(input_frame,
                  text="Ecuación",
                  **style.STYLE_SUBTITTLE,
                  ).grid(row=0, column=0)
 
         # label exponente
-        tk.Label(inputFrame,
-                 text="Exponente",
+        tk.Label(input_frame,
+                 text="Exponente (x)",
                  **style.STYLE_SUBTITTLE,
                  ).grid(row=1, column=0)
 
         # label respuesta
-        tk.Label(inputFrame,
+        tk.Label(input_frame,
                  text="Respuesta",
                  **style.STYLE_SUBTITTLE,
                  ).grid(row=2, column=0)
 
         # label ecuacion usada
-        tk.Label(inputFrame,
+        tk.Label(input_frame,
                  text="(e^x)/(e^x)-1",
                  **style.STYLE_SUBTITTLE,
-                 ).grid(row=0, column=1, pady="10", padx="20")
-
-        # entry desactivado respuesta
-        self.respuesta = tk.StringVar()
-        tk.Entry(inputFrame,
-                 textvariable=self.respuesta,
-                 **style.STYLE_ENTRY_DES,
-                 ).grid(row=2, column=1, pady="10", padx="20")
+                 ).grid(row=0, column=1, columnspan=2, pady=10, padx=(20, 0))
 
         # entry exponente
+        borde_entry_1 = tk.LabelFrame(input_frame, **style.STYLE_ENTRY_BORDER)
+        borde_entry_1.grid(row=1, column=1, columnspan=2,
+                           pady=10, padx=(20, 0), sticky=tk.EW)
+
         self.exponente = tk.StringVar()
-        tk.Entry(inputFrame,
+        tk.Entry(borde_entry_1,
                  textvariable=self.exponente,
                  **style.STYLE_ENTRY,
-                 ).grid(row=1, column=1, pady="10", padx="20", sticky=tk.EW)
+                 ).pack(side=tk.TOP, expand=True)
+
+        canvas_linea_1 = tk.Canvas(
+            borde_entry_1, **style.STYLE_CANVAS, width=250)
+        canvas_linea_1.pack(side=tk.TOP, anchor=tk.CENTER)
+        canvas_linea_1.create_line(0, 0, 250, 0, **style.STYLE_CANVAS_LINE)
+
+        # label alerta exponente
+        self.texto_alerta_exponente = tk.StringVar()
+        tk.Label(borde_entry_1,
+                 textvariable=self.texto_alerta_exponente,
+                 **style.STYLE_WARNING
+                 ).pack()
+
+        # entry desactivado respuesta
+        borde_entry_2 = tk.LabelFrame(input_frame, **style.STYLE_ENTRY_BORDER)
+        borde_entry_2.grid(row=2, column=1, columnspan=2,
+                           pady=10, padx=(20, 0), sticky=tk.EW)
+
+        self.respuesta = tk.StringVar()
+        tk.Entry(borde_entry_2,
+                 textvariable=self.respuesta,
+                 **style.STYLE_ENTRY_DES,
+                 ).pack(side=tk.TOP, expand=True)
+
+        canvas_linea_2 = tk.Canvas(
+            borde_entry_2, **style.STYLE_CANVAS, width=250)
+        canvas_linea_2.pack(side=tk.TOP, anchor=tk.CENTER)
+        canvas_linea_2.create_line(0, 0, 250, 0, **style.STYLE_CANVAS_LINE)
 
         # boton para calcular
-        borde_1 = tk.LabelFrame(inputFrame,
-                                **style.STYLE_BUTTON_BORDER)
-        borde_1.grid(row=0, rowspan=3, column=2, pady="30", padx="20")
+        borde_1 = tk.LabelFrame(input_frame, **style.STYLE_BUTTON_BORDER)
+        borde_1.grid(row=0, rowspan=3, column=3, pady="30", padx="20")
 
         boton_calculo = tk.Button(borde_1,
                                   text="Calcular",
@@ -97,7 +141,32 @@ class PropErrores(tk.Frame):
         boton_calculo.bind('<Enter>', event.on_enter)
         boton_calculo.bind('<Leave>', event.on_leave)
 
-        outputFrame = tk.Frame(self, background="#666666")
-        outputFrame.pack(side=tk.TOP, fill=tk.BOTH,
-                         expand=True, padx="10", pady="10")
+        output_frame = tk.Frame(self, background=style.BG)
+        output_frame.pack(side=tk.TOP, fill=tk.BOTH,
+                          expand=True, padx=10, pady=10)
+        
+        # label titulo de la gráfica
+        tk.Label(output_frame,
+                 text="Función e^x/(e^x)-1",
+                 **style.STYLE_SUBTITTLE,
+                 ).pack(side=tk.TOP, fill=tk.X, expand=True)
+        
+        self.fig = matplotlib.figure.Figure(facecolor=style.BG)
+        self.ax = self.fig.add_subplot(facecolor= style.BG)
+
+        self.ax.spines['bottom'].set_color(style.COLOR_AQUA)
+        self.ax.spines['bottom'].set_linewidth(2)
+        self.ax.spines['left'].set_color(style.COLOR_AQUA)
+        self.ax.spines['left'].set_linewidth(2)
+        self.ax.spines['right'].set_color(style.COLOR_AQUA)
+        self.ax.spines['right'].set_linewidth(2)
+        self.ax.spines['top'].set_color(style.COLOR_AQUA)
+        self.ax.spines['top'].set_linewidth(2)
+
+        self.ax.tick_params(axis='both', colors= style.COLOR_AQUA, labelsize=10, size= 7, width=2)
+
+        self.canvas_figura = FigureCanvasTkAgg(self.fig, master=output_frame)
+        self.canvas_figura.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH,
+                          expand=True)
+        
         
