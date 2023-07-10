c43d0e31ae4e1b4338c69f757af6b0a956c77d19
ReitanSora <stivenpilca@gmail.com>
2023-07-10 13:29:04 -0500

    Se retira opción innecesaria del menú

--- Proyecto Final/modules/menu.py
+++ Proyecto Final/modules/menu.py
@@ -24,11 +24,9 @@ class Menu(tk.Menu):
 
         opcion_user = tk.Menu(barra_menu, **style.STYLE_MENU_BAR)
         opcion_file = tk.Menu(barra_menu, **style.STYLE_MENU_BAR)
-        opcion_edit = tk.Menu(barra_menu, **style.STYLE_MENU_BAR)
         opcion_help = tk.Menu(barra_menu, **style.STYLE_MENU_BAR)
 
         barra_menu.add_cascade(label="File", menu=opcion_file)
-        barra_menu.add_cascade(label="Edit", menu=opcion_edit)
         barra_menu.add_cascade(label="User", menu=opcion_user)
         barra_menu.add_cascade(label="Help", menu=opcion_help)
 
@@ -36,9 +34,6 @@ class Menu(tk.Menu):
         opcion_file.add_command(label="Nueva ventana", command=parent.new_window)
         opcion_file.add_separator()
         opcion_file.add_command(label="Salir" , command=parent.salir)
-
-        opcion_edit.add_command(label="Copiar", command='Ctrl+Z')
-        opcion_edit.add_command(label="Cortar")
 
         opcion_user.add_command(label="Actualizar Información")
         opcion_user.add_command(label="Cambiar Clave")
